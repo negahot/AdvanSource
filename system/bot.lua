b49659c177bcb5685405aec1e88832abc74c3fa0
negahot <clashroyale1000gem@gmail.com>
2016-10-15 21:03:47 +0330

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -234,18 +234,12 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
-    support_gp = {},--Support id
+    sudo_users = {275387751},--Sudo users
+    support_gp = {164383660},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
 This is an original bot and based on (AdvanSource).
 Copyright all right reserved and you must respect all laws.
-
-Source: https://github.com/janlou/AdvanSource
-Channel: @AdvanTm
-Messenger: @Advanbot
-Creator: @janlou
-Site: http://StoreVps.ir
 ]],
     --Start rate:
     Group_rate = [[]],
